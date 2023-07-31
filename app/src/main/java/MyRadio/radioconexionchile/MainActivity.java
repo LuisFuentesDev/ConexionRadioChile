a937760eed8305aa9ca7b182c25bfe261002ffb3
LuisFuentesDev <lsfuentes2@gmail.com>
2023-07-31 17:49:04 -0400

    Colores nuevos

--- app/src/main/java/MyRadio/radioconexionchile/MainActivity.java
+++ app/src/main/java/MyRadio/radioconexionchile/MainActivity.java
@@ -6,13 +6,17 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import MyRadio.radioconexionchile.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
+    BottomNavigationView bottomNavigationView;
 
     @SuppressLint("NonConstantResourceId")
     @Override
@@ -20,25 +24,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
         replaceFragment(new HomeFragment());
+
 
         binding.bottomNavigationView.setOnItemSelectedListener(item -> {
             if (item.getItemId() == R.id.home) {
                 replaceFragment(new HomeFragment());
             } else if (item.getItemId() == R.id.explore) {
                 replaceFragment(new ExploreFragment());
-            }  else replaceFragment(new SearchFragment());
+            } else replaceFragment(new SearchFragment());
+
+
+            bottomNavigationView.setBackgroundColor(Color.parseColor("#4E2593")); // Barra de navegacion
+            getWindow().setNavigationBarColor(Color.parseColor("#000000")); // Barra Inferior
+            getWindow().setStatusBarColor(Color.parseColor("#4E2593")); // Barra superior
 
             return true;
         });
     }
-    private void replaceFragment(Fragment fragment){
+
+    private void replaceFragment(Fragment fragment) {
 
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.frameLayout, fragment);
         fragmentTransaction.commit();
 
-
     }
+
 }
